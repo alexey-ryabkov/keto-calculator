2dad17a624f7e11c7f4d5a3a59dfafd153c48578
alexey-ryabkov <xelaris@ya.ru>
2025-08-06 00:41:57 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/keto/calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.verygoodcore.keto_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:keto_calculator/app/app.dart';
+import 'package:keto_calculator/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
